d6c3957384cc9f2decb317533e89549d63d5ca66
Freddy Söderlund <62197952+soderlundf@users.noreply.github.com>
2024-09-14 17:28:51 +0200

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker && sudo systemctl enable docker
+docker run hello-world
